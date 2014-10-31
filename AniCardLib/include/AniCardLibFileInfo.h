003d7706089b2489511ca027b10f5cdf41a8d915
Andrew Huertas <ahuertas@student.neumont.edu>
2014-10-31 04:01:35 +0000

    git-svn-id: https://svn.neumont.edu:8443/svn/capstone_andrew_huertas/trunk@61 4a9086dd-1ff8-f640-9482-caef19e4c562

---
+++ AniCardLib/include/AniCardLibFileInfo.h
@@ -0,0 +1,11 @@
+#include <AniCardLibExportHeader.h>
+
+class ANICARDLIB_SHARED AniCardLibFileInfo
+{
+	unsigned int numCards;
+	unsigned int numModelData;
+	
+	void* cardData;
+	void* modelData;
+
+};
